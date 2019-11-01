d328c930a57454c15c3daf6c86183d6e8fc29a09
oradkovsky <ostap.radkovskyy@gmail.com>
2019-11-01 14:59:01 +0200

    impl: update with soft zigzag capability

--- library/src/main/java/ir/beigirad/zigzagview/ZigzagView.java
+++ library/src/main/java/ir/beigirad/zigzagview/ZigzagView.java
@@ -10,6 +10,7 @@ import android.graphics.Paint.Style;
 import android.graphics.Path;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Build;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
@@ -18,7 +19,6 @@ import android.renderscript.ScriptIntrinsicBlur;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.FrameLayout;
 
 import static android.graphics.Bitmap.Config.ALPHA_8;
@@ -29,6 +29,10 @@ import static android.graphics.PorterDuff.Mode.SRC_IN;
 public class ZigzagView extends FrameLayout {
     private static final int ZIGZAG_TOP = 1;
     private static final int ZIGZAG_BOTTOM = 2; // default to be backward compatible.Like google ;)
+    private static final int ZIGZAG_RIGHT = 4;
+
+    private static final int ZIGZAG_TYPE_SHARP = 1;
+    private static final int ZIGZAG_TYPE_SOFT = 3;
 
     private int zigzagHeight;
     private int zigzagElevation;
@@ -40,6 +44,7 @@ public class ZigzagView extends FrameLayout {
     private int zigzagPaddingTop;
     private int zigzagPaddingBottom;
     private int zigzagSides;
+    private int zigzagType;
     private float zigzagShadowAlpha;
 
     private Path pathZigzag = new Path();
@@ -87,6 +92,7 @@ public class ZigzagView extends FrameLayout {
         zigzagPaddingTop = (int) a.getDimension(R.styleable.ZigzagView_zigzagPaddingTop, zigzagPadding);
         zigzagPaddingBottom = (int) a.getDimension(R.styleable.ZigzagView_zigzagPaddingBottom, zigzagPadding);
         zigzagSides = a.getInt(R.styleable.ZigzagView_zigzagSides, ZIGZAG_BOTTOM);
+        zigzagType = a.getInt(R.styleable.ZigzagView_zigzagType, ZIGZAG_TYPE_SOFT);
         zigzagShadowAlpha = a.getFloat(R.styleable.ZigzagView_zigzagShadowAlpha, 0.5f);
         a.recycle();
 
@@ -110,10 +116,13 @@ public class ZigzagView extends FrameLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         rectMain.set(0, 0, getMeasuredWidth(), getMeasuredHeight());
-        rectZigzag.set(rectMain.left + zigzagPaddingLeft, rectMain.top + zigzagPaddingTop, rectMain.right - zigzagPaddingRight, rectMain.bottom - zigzagPaddingBottom);
+        rectZigzag.set(rectMain.left + zigzagPaddingLeft,
+                rectMain.top + zigzagPaddingTop,
+                rectMain.right - zigzagPaddingRight,
+                rectMain.bottom - zigzagPaddingBottom);
         rectContent.set(rectZigzag.left + zigzagPaddingContent,
                 rectZigzag.top + zigzagPaddingContent + (containsSide(zigzagSides, ZIGZAG_TOP) ? zigzagHeight : 0),
-                rectZigzag.right - zigzagPaddingContent,
+                rectZigzag.right - zigzagPaddingContent - (containsSide(zigzagSides, ZIGZAG_RIGHT) ? zigzagHeight : 0),
                 rectZigzag.bottom - zigzagPaddingContent - (containsSide(zigzagSides, ZIGZAG_BOTTOM) ? zigzagHeight : 0));
 
         super.setPadding(rectContent.left, rectContent.top, rectMain.right - rectContent.right, rectMain.bottom - rectContent.bottom);
@@ -138,20 +147,39 @@ public class ZigzagView extends FrameLayout {
         float top = rectZigzag.top;
         float bottom = rectZigzag.bottom;
 
-        pathZigzag.moveTo(right, bottom);
-        pathZigzag.lineTo(right, top);
+        pathZigzag.moveTo(right, top);
 
-        if (containsSide(zigzagSides, ZIGZAG_TOP))
-            drawHorizontalSide(pathZigzag, left, top, right, true);
-        else
+        if (containsSide(zigzagSides, ZIGZAG_TOP)) {
+            if (zigzagType == ZIGZAG_TYPE_SHARP) {
+                drawHorizontalSide(pathZigzag, left, top, right, true);
+            } else {
+                drawHorizontalSideSoft(pathZigzag, left, top, right, bottom, true);
+            }
+        } else {
             pathZigzag.lineTo(left, top);
+        }
 
         pathZigzag.lineTo(left, bottom);
 
         if (containsSide(zigzagSides, ZIGZAG_BOTTOM))
-            drawHorizontalSide(pathZigzag, left, bottom, right, false);
-        else
+            if (zigzagType == ZIGZAG_TYPE_SHARP) {
+                drawHorizontalSide(pathZigzag, left, bottom, right, false);
+            } else {
+                drawHorizontalSideSoft(pathZigzag, left, top, right, bottom, false);
+            }
+        else {
             pathZigzag.lineTo(right, bottom);
+        }
+
+        if (containsSide(zigzagSides, ZIGZAG_RIGHT)) {
+            if (zigzagType == ZIGZAG_TYPE_SHARP) {
+                drawVerticalSide(pathZigzag, left, top, right, bottom, true);
+            } else {
+                drawVerticalSideSoft(pathZigzag, left, top, right, bottom, true);
+            }
+        } else {
+            pathZigzag.lineTo(right, top);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -172,6 +200,100 @@ public class ZigzagView extends FrameLayout {
         input.destroy();
         output.destroy();
 
+    }
+
+    private void drawHorizontalSideSoft(Path path, float left, float top, float right, float bottom, boolean isTop) {
+        int h = zigzagHeight;
+        int seed = 2 * h;
+        int width = (int) (right - left);
+        int count = width / seed;
+        int diff = width - (seed * count);
+        int sideDiff = diff / 2;
+
+        if (isTop) {
+            float endX;
+            float startX = 0;
+
+            for (int i = count; i > 0; i--) {
+                if (i == count) {
+                    endX = right;
+                    startX = endX - seed - sideDiff;
+                } else if (i == 1) {
+                    startX = left;
+                    endX = startX + seed + sideDiff;
+                } else {
+                    endX = startX;
+                    startX = endX - seed;
+                }
+
+                path.arcTo(new RectF(startX, top - h, endX, top + h), 0, 180);
+            }
+        } else {
+            float endX = 0;
+            float startX;
+
+            for (int i = 0; i < count; i++) {
+                if (i == 0) {
+                    startX = left;
+                    endX = startX + seed + sideDiff;
+                } else if (i == count - 1) {
+                    endX = right;
+                    startX = endX - seed - sideDiff;
+                } else {
+                    startX = endX;
+                    endX = startX + seed;
+                }
+
+                path.arcTo(new RectF(startX, bottom - h, endX, bottom + h), 180, 180);
+            }
+        }
+    }
+
+    private void drawVerticalSideSoft(Path path, float left, float top, float right, float bottom, boolean isRight) {
+        int h = zigzagHeight;
+        int seed = 2 * h;
+        int width = (int) (bottom - top);
+        int count = width / seed;
+        int diff = width - (seed * count);
+        int sideDiff = diff / 2;
+
+        if (isRight) {
+            float endY;
+            float startY = 0;
+
+            for (int i = count; i > 0; i--) {
+                if (i == count) {
+                    endY = bottom;
+                    startY = endY - seed - sideDiff;
+                } else if (i == 1) {
+                    startY = top;
+                    endY = startY + seed + sideDiff;
+                } else {
+                    endY = startY;
+                    startY = endY - seed;
+                }
+
+                path.arcTo(new RectF(right - h, startY, right + h, endY), 90, 180);
+            }
+        } else {
+            float endX = 0;
+            float startX;
+
+            for (int i = 0; i < count; i++) {
+                if (i == 0) {
+                    startX = left;
+                    endX = startX + seed + sideDiff;
+                } else if (i == count - 1) {
+                    endX = right;
+                    startX = endX - seed - sideDiff;
+                } else {
+                    startX = endX;
+                    endX = startX + seed;
+                }
+
+                path.arcTo(new RectF(startX, bottom - h, endX, bottom + h), 180, 180);
+            }
+        }
     }
 
     private void drawHorizontalSide(Path path, float left, float y, float right, boolean isTop) {
@@ -212,8 +334,57 @@ public class ZigzagView extends FrameLayout {
                 path.lineTo(endSeed, y);
             }
         }
+    }
 
+    private void drawVerticalSide(Path path, float left, float top, float right, float bottom, boolean isRight) {
+        int h = zigzagHeight;
+        int seed = 2 * h;
+        int width = (int) (bottom - top);
+        int count = width / seed;
+        int diff = width - (seed * count);
+        int sideDiff = diff / 2;
 
+        float halfSeed = (float) (seed / 2);
+
+        if (isRight) {
+            float destY1 = 0;
+            float destY2 = 0;
+
+            for (int i = count; i > 0; i--) {
+                if (i == count) {
+                    destY1 = bottom - halfSeed - sideDiff;
+                    destY2 = destY1 - halfSeed;
+                } else if (i == 1) {
+                    destY1 = top + halfSeed + sideDiff;
+                    destY2 = top;
+                } else {
+                    destY1 = destY1 - seed;
+                    destY2 = destY1 - halfSeed;
+                }
+
+                path.lineTo(right - h, destY1);
+                path.lineTo(right, destY2);
+            }
+        } else {
+            float destY1 = 0;
+            float destY2 = 0;
+
+            for (int i = 0; i < count; i++) {
+                if (i == 0) {
+                    destY1 = top + halfSeed + sideDiff;
+                    destY2 = destY1 + halfSeed;
+                } else if (i == 1) {
+                    destY1 = bottom - halfSeed - sideDiff;
+                    destY2 = bottom;
+                } else {
+                    destY1 = destY1 + seed;
+                    destY2 = destY1 + halfSeed;
+                }
+
+                path.lineTo(left + h, destY1);
+                path.lineTo(left, destY2);
+            }
+        }
     }
 
     private boolean containsSide(int flagSet, int flag) {
